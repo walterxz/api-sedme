285a4f471758f901e3ebc191d10674c0c818dd7d
Walter Miguel Diaz Jaramillo <waltz.mx@gmail.com>
2020-09-09 20:24:05 -0500

    CORS access

--- src/app.js
+++ src/app.js
@@ -11,7 +11,24 @@ import rolRoutes from './routes/roles';
 
 //initalization
 const app = express();
-
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+   let allowedOrigins = ["http://10.72.5.47:3000", "http://localhost:8081"];
+   let origin = req.headers.origin;
+   if (allowedOrigins.indexOf(origin) > -1) {
+     // console.log(`Access from ${origin}`);
+     res.setHeader("Access-Control-Allow-Origin", origin);
+   } else {
+     console.log("Access ip denied");
+   }
+   res.setHeader(
+     "Access-Control-Allow-Methods",
+     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+   );
+   res.setHeader("Access-Control-Allow-Headers","X-Requested-With,content-type");
+   res.setHeader("Access-Control-Allow-Credentials", true);
+   next();
+ });
 const swaggerDoc = require('../swagger.json');
 //middlewares
 app.use(morgan('dev')); //Va mostrando las peticiones por consola
